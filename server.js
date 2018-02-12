af5566b2908faa4a681e030b45ccd89313097b12
kjacks <kjack227@gmail.com>
2018-02-12 12:13:31 -0500

    modified start script

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/dist/'));
+
+app.get('*', function(req, res){
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+app.listen(process.env.PORT || 3000, function () {
+    var port = process.env.PORT || 3000;
+    console.log("App now running on port", port);
+});
