1043ad92c3b26a4e0e4ad487ba89070bda702f08
kjacks <kjack227@gmail.com>
2018-04-18 10:14:51 -0400

    fixed webpack config syntax error

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -14,7 +14,7 @@ const GLOBALS = {
     API_AUDIENCE: JSON.stringify(process.env.API_AUDIENCE),
     AUTH0_CLIENT_ID: JSON.stringify(process.env.AUTH0_CLIENT_ID),
     CALLBACK_URL: JSON.stringify(process.env.CALLBACK_URL)
-  }
+  },
   __DEV__: false
 };
 
