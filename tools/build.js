5887435e0726fe81e8657a9460bf5c21f9990e26
kjacks <kjack227@gmail.com>
2018-04-17 21:43:27 -0400

    remove duplicate declaration of config

--- tools/build.js
+++ tools/build.js
@@ -2,12 +2,11 @@
 // Allowing console calls below since this is a build file.
 /* eslint-disable no-console */
 import webpack from 'webpack';
-import config from '../webpack.config.prod';
 // import {chalkError, chalkSuccess, chalkWarning, chalkProcessing} from './chalkConfig';
 
 // process.env.NODE_ENV = 'production'; // this assures React is built in prod mode and that the Babel dev config doesn't apply.
 let config
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   config = require('../webpack.config.prod')
 } else {
   config = require('../webpack.config.dev')
